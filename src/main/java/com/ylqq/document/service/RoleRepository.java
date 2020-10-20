d55ae3e5854c7dbbd0d20dc3cf350af6f9029e60
6ylqq <937192804@qq.com>
2020-10-20 21:42:09 +0800

    简化了Role的全部函数，以使用Repository为主

--- src/main/java/com/ylqq/document/service/RoleRepository.java
+++ src/main/java/com/ylqq/document/service/RoleRepository.java
@@ -1,7 +1,10 @@
 package com.ylqq.document.service;
 
+import com.ylqq.document.pojo.Function;
 import com.ylqq.document.pojo.Role;
 import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.ArrayList;
 
 /**
  * @author ylqq
